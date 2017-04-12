e4eb2a07089a5365e720cb19d1408659fd4326da
Michael Schuler <schuler5044@mydusd.org>
2017-04-12 12:01:54 -0700

    Added the abstract class and the main class

---
+++ Battleship.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Battleship here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Battleship  
+{
+    public void shoot();
+    public void move();
+    public void sink();
+   
+}
---
+++ Play.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Play here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Play implements Battleship
+{
+    public void sink(){}
+    public void move(){}
+    public void shoot(){}
+    }
+
