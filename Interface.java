9a5b75fe94efb50ed2d9bb9c06be387d6db312ca
Michael Schuler <schuler5044@mydusd.org>
2017-04-12 17:44:36 -0700

    Made a moving ship and added the background

---
+++ Interface.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Play here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Interface implements Battleship
+{
+    public void sink(){}
+    public void move(){}
+    public void shoot(){}
+    }
+
